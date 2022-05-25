f8ef8279fa0fc8055cc164a8e6d6cb3668953f94
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-05-25 14:01:49 -0300

    chore: add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
-const WebpackShellPluginNext = require('webpack-shell-plugin-next')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-const {
-  NODE_ENV = 'production',
-} = process.env
+const { NODE_ENV = 'production' } = process.env
 
 module.exports = {
-  entry: './src/index.ts',  // the file you would provide to ts-node or node binaries for execution
-  mode: NODE_ENV,  // development or production
-  target: 'node',  // webpack works differently based on target, here we use node.js
-  output: {  // directions for the built files directory
+  entry: './src/index.ts', // the file you would provide to ts-node or node binaries for execution
+  mode: NODE_ENV, // development or production
+  target: 'node', // webpack works differently based on target, here we use node.js
+  output: {
+    // directions for the built files directory
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
   },
@@ -29,9 +28,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,   // this rule will only activate for files ending in .ts
+        test: /\.ts$/, // this rule will only activate for files ending in .ts
         use: [{ loader: 'ts-loader' }],
-        exclude: [  // exclude any files you don't want to include, eg test files
+        exclude: [
+          // exclude any files you don't want to include, eg test files
           /__tests__/,
         ],
       },
@@ -39,11 +39,11 @@ module.exports = {
   },
   externals: [nodeExternals()],
   plugins: [
-    new WebpackShellPluginNext({
-      onBuildStart: {
-       
-        blocking: true
-      }
-    })
+    new BundleAnalyzerPlugin({
+      reportFilename: 'bundle_analysis.html',
+      generateStatsFile: true,
+      openAnalyzer: false,
+      analyzerMode: 'static',
+    }),
   ],
 }
