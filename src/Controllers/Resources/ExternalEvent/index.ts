612936acf5161f62723db8bed2d99ac907baa458
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-10 11:05:38 -0300

    chore: sms diminish

--- src/Controllers/Resources/ExternalEvent/index.ts
+++ src/Controllers/Resources/ExternalEvent/index.ts
@@ -128,10 +128,10 @@ class ExternalEvent {
 
           const { TEMPLATES, send } = new SendgridClient()
 
-          /*   Twillio.sendSimpleMessage(
-            `Olá, ${name}! Tudo certo com a sua inscrição para os 13 anos da Gênesis Church! #13anosgenesis`,
+          Twillio.sendSimpleMessage(
+            `Ola, ${name}! Esperamos voce para os 13 anos da Genesis Church! #13anosgenesis`,
             phone,
-          ) */
+          )
 
           await send(TEMPLATES.anniversary.config(email, {}))
 
