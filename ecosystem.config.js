52d300aad8977cc231cb560c3c0b9b435bc3c228
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-14 17:05:02 -0300

    chore: add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: `genesis-backend`,
+      script: './dist/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'localhost',
+      },
+      env_development: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_staging: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+    },
+  ],
+}
