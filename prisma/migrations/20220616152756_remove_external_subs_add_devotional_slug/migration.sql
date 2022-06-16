8cb2ca3c471c093227c4000ff770351cb8d4e231
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-16 12:28:09 -0300

    chore: new migrations

---
+++ prisma/migrations/20220616152756_remove_external_subs_add_devotional_slug/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ExternalEvent` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ExternalSubscriptions` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[slug]` on the table `Devotional` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE "ExternalSubscriptions" DROP CONSTRAINT "ExternalSubscriptions_externalEventId_fkey";
+
+-- DropTable
+DROP TABLE "ExternalEvent";
+
+-- DropTable
+DROP TABLE "ExternalSubscriptions";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Devotional_slug_key" ON "Devotional"("slug");
