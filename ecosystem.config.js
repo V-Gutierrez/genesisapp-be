047fda2a015ab9df9edb5fa19548a94579b434da
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-06-17 09:17:13 -0300

    chore: add heroku optimizations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: './dist/index.js',
       instances: '4',
       exec_mode: 'cluster',
+      node_args: '--optimize_for_size --max_old_space_size=460 --gc_interval=100',
       env: {
         NODE_ENV: 'localhost',
       },
