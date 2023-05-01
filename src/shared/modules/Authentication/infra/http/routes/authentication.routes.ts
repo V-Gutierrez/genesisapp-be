c3ad29aca2a0d40cf7c45b1df0c045df63f36bcf
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-04-30 23:07:03 -0300

    events y gcs

--- src/shared/modules/Authentication/infra/http/routes/authentication.routes.ts
+++ src/shared/modules/Authentication/infra/http/routes/authentication.routes.ts
@@ -1,5 +1,5 @@
-import Middlewares from 'src/shared/infra/http/middlewares'
-import AuthenticationController from 'src/shared/modules/Authentication/infra/http/controllers/AuthenticationController'
+import Middlewares from '@Shared/infra/http/middlewares'
+import AuthenticationController from '@Shared/modules/Authentication/infra/http/controllers/AuthenticationController'
 import { Router } from 'express'
 
 const AuthenticationRouter = Router()
@@ -10,22 +10,14 @@ AuthenticationRouter.route('/auth').get(
   Middlewares.Authentication,
   AuthenticationController.refreshToken,
 )
-AuthenticationRouter.route('/auth/activate').post(
-  AuthenticationController.activateNewUser,
-)
+AuthenticationRouter.route('/auth/activate').post(AuthenticationController.activateNewUser)
 
-AuthenticationRouter.route('/auth/reset-password').post(
-  AuthenticationController.resetPassword,
-)
-AuthenticationRouter.route('/auth/reset-password').put(
-  AuthenticationController.setNewPassword,
-)
+AuthenticationRouter.route('/auth/reset-password').post(AuthenticationController.resetPassword)
+AuthenticationRouter.route('/auth/reset-password').put(AuthenticationController.setNewPassword)
 AuthenticationRouter.route('/auth/logout').delete(
   Middlewares.Authentication,
   AuthenticationController.logout,
 )
-AuthenticationRouter.route('/auth/me').get(
-  AuthenticationController.getUserInformation,
-)
+AuthenticationRouter.route('/auth/me').get(AuthenticationController.getUserInformation)
 
 export default AuthenticationRouter
