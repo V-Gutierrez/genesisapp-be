f09b5940d9e03bdae7a07ac818878bdec852c53b
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2023-05-10 14:15:14 -0300

    chore: add validation

--- src/shared/helpers/SchemaHelper.ts
+++ src/shared/helpers/SchemaHelper.ts
@@ -3,7 +3,7 @@ import * as R from 'ramda'
 import Joi, { Schema } from 'joi'
 
 class SchemaHelper {
-  private readonly weekdays = [
+  private readonly WEEKDAYS = [
     'Domingo',
     'Segunda-feira',
     'Terça-feira',
@@ -11,6 +11,35 @@ class SchemaHelper {
     'Quinta-feira',
     'Sexta-feira',
     'Sábado',
+  ]
+
+  private readonly WORKING_HOURS = [
+    '08:00',
+    '08:30',
+    '09:00',
+    '09:30',
+    '10:00',
+    '10:30',
+    '11:00',
+    '11:30',
+    '12:00',
+    '12:30',
+    '13:00',
+    '13:30',
+    '14:00',
+    '14:30',
+    '15:00',
+    '15:30',
+    '16:00',
+    '16:30',
+    '17:00',
+    '17:30',
+    '18:00',
+    '18:30',
+    '19:00',
+    '19:30',
+    '20:00',
+    '20:30',
   ]
 
   SIGNUP_SCHEMA = Joi.object().keys({
@@ -60,8 +89,8 @@ class SchemaHelper {
     name: Joi.string().required(),
     whatsappLink: Joi.string().required(),
     addressInfo: Joi.string().required(),
-    weekDay: Joi.allow(this.weekdays).required(),
-    scheduledTime: Joi.string().required(),
+    weekDay: Joi.allow(this.WEEKDAYS).required(),
+    scheduledTime: Joi.allow(this.WORKING_HOURS).required(),
     leadership: Joi.array().min(2).items(Joi.string()).required(),
   })
 
